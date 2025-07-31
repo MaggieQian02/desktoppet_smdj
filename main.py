42212e039af57e08e1134ceae84a9d018e3230a5
Youqi Qian <yq270@cam.ac.uk>
2025-07-31 13:44:07 +0800

    Updated function of moving, click and drag

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ import os
 class l_Rloe(Screen): # character
     def __init__(self):
         super().__init__()
-        self.pos = 50,30
+        self.pos = 10,10
         self.size_hint = (None, None) # ratio
-        self.size = Vector(2360, 1640)*0.25
+        self.size = Vector(2360, 1640)*0.22
         
         _default_line = "default" # file location for animated png
         self._default_file_list = [os.path.join(_default_line, i) for i in os.listdir(_default_line)]
@@ -26,13 +26,18 @@ class l_Rloe(Screen): # character
         with self.canvas:
             self.update_img = Rectangle(size=self.size)
 
-    # movement
-    def default(self, ints): # move horizontally
+    # animation
+    def default(self, ints): # default jump
         self.update_img.source = self.check_list[self.s_index] # select file
-        self.s_index += 1
-
+        
+        self.x +=1 # move horizontally to the right
+        if self.x >= self.parent.width: # come back from the left if it goes right out of the screen
+            self.x = -self.width
+        
+        self.s_index += 1 # plays the next frame from the png files
         if self.s_index >= len(self.check_list)-0: # if number greater than the total number of files, 
             self.s_index = 0 # go back to 0
+
 
 class MainApp(App): # background screen
     def __init__(self): # initialise
@@ -44,6 +49,15 @@ class MainApp(App): # background screen
 
         Window.borderless = True 
 
+        Window.bind(on_touch_move = self.move_touch)
+
+    def move_touch(self, window, touch):
+        #print(window)
+        #print(touch)
+        self.l_rloe.x = touch.x - self.l_rloe.width/2
+        self.l_rloe.y = touch.y - self.l_rloe.height/2
+
+
     def build(self):
         return self.body
     
@@ -51,16 +65,15 @@ class MainApp(App): # background screen
         hwnd = win32gui.GetForegroundWindow() # gets Handle
         window_hwnd = win32gui.GetDesktopWindow() # size of desktop
         left, top, right, botton = win32gui.GetWindowRect(window_hwnd) # position on desktop
-        win32gui.MoveWindow(hwnd, left, top, right, botton, False) # change the size of the screen
+        win32gui.MoveWindow(hwnd, left, top, right, botton, False) # change the size of the screen to fit desktop size
         
         ex_style = win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) 
         win32api.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, ex_style | win32con.WS_EX_LAYERED) 
         
         win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0, 0, 0), 0, win32con.LWA_COLORKEY) # make the screen transparent
 
-        Clock.schedule_interval(self.l_rloe.default, 1/8)
+        Clock.schedule_interval(self.l_rloe.default, 1/8) # animation play speed: seconds per frame
 
-        #self.l_rloe.default(1)
     
 # run
 if __name__ == '__main__': 
