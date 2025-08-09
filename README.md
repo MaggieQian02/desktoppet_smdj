81413c3ebd9761441db084916856452228176827
Youqi Qian <yq270@cam.ac.uk>
2025-08-09 16:42:56 +0800

    Added new animation pngs in a new folder directory. New function in progress: button to select wanted animations (created right-clicking and key board input; created list of buttons to be pressed; stopped interference between clicking button and animation clicking cycle)

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 This is a Python project of a desktop pet of my favourite characters Shimok and Dongjae from the K-drama Stranger.
 
-The code is based on kivy package, referenced to this tutorial https://www.bilibili.com/video/BV1H1QPYJEAA/?spm_id_from=333.1007.top_right_bar_window_default_collection.content.click&vd_source=c4d81b01f0d7accc2eea854dc487972a.
+The code is based on kivy package, referenced to this tutorial https://www.bilibili.com/video/BV1H1QPYJEAA/?spm_id_from=333.1007.top_right_bar_window_default_collection.content.click&vd_source=c4d81b01f0d7accc2eea854dc487972a, with some help from ChatGPT.
 
-The animations are drawn by myself referenced to the templates from http://xhslink.com/m/30P4Oo5BbT7, and http://xhslink.com/m/63HFQjpeynt.
+The animations are drawn by myself referenced to the templates from http://xhslink.com/m/30P4Oo5BbT7,  http://xhslink.com/m/7nlS5ITJaUR, http://xhslink.com/m/37EpkTHceMD, and http://xhslink.com/m/63HFQjpeynt.
 
-More functions of user interaction are being created...
+Functions include:
+    - On default animation, one click to stop moving, click again to stop animating; hold and drag
+    - Select and switch to different animations
+    And maybe more to come...
+
+It is also planed to record a video for user instruction.
--- main.py
+++ main.py
@@ -28,11 +28,48 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen # uses the screen in terminal
 from kivy.core.window import Window
 from kivy.vector import Vector
-from kivy.graphics import Rectangle
+from kivy.graphics import Rectangle, BoxShadow, Color
 from kivy.clock import Clock # for repitition
+from kivy.uix.button import Button
+from kivy.animation import Animation
+from kivy.utils import get_color_from_hex
+
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,disable_multitouch')
 
 import win32api, win32con, win32gui # gets the desktop
 import os
+
+
+class Select_Button(Button):
+    def __init__(self, **kwargs):
+        super(Select_Button, self).__init__()
+
+        self.size = (70, 70)
+
+        with self.canvas.before:
+            # hex colour code: red = #781e18, blue = #121c3a, pink = #e99994, light blue = #39529c
+            Color(*get_color_from_hex("#39529c"))
+            self.rect = BoxShadow(size=self.size, border_radius=(45,45,45,45), inset=True, blur_radius=50)
+        self.bind(pos=self.update_canvas)
+
+        for key,values in kwargs.items():
+            self.__setattr__(key, values) 
+
+        self.background_color = (0.2, 0.2, 0.2, 0)
+        self.size_hint = (None, None)
+
+        self.start_an = Animation(blur_radius=0)
+        self.end_an = Animation(blur_radius=100)
+        
+    def update_canvas(self, widget, pos):
+        self.rect.pos = pos
+
+    def on_press(self):
+        self.start_an.start(self.rect)
+
+    def on_release(self):
+        self.end_an.start(self.rect)
 
 
 class l_Rloe(Screen): # character
@@ -40,11 +77,15 @@ class l_Rloe(Screen): # character
         super().__init__()
         self.pos = 10,10
         self.size_hint = (None, None) # ratio
-        self.size = Vector(2360, 1640)*0.22
+        self.size = Vector(2360, 1640)*0.2
 
-        _default_line = "default" # file location for animated png
+        # file locations for animated png
+        _default_line = "default" 
         self._default_file_list = [os.path.join(_default_line, i) for i in os.listdir(_default_line)]
-        print(self._default_file_list)
+        #print(self._default_file_list)
+
+        _working_line = "working"
+        self._working_file_list = [os.path.join(_working_line, i) for i in os.listdir(_working_line)]
 
         self.animating = True        # For frame animation
         self.moving_right = True     # For horizontal motion
@@ -52,28 +93,42 @@ class l_Rloe(Screen): # character
 
         self.s_index = 0
         self.check_list = self._default_file_list
+        #self.check_list = self._working_file_list
 
         with self.canvas:
             self.update_img = Rectangle(size=self.size)
 
-    # animation
-    def default(self, dt): # default jump
-        if not self.animating:
-            return
-
-        # Update image
-        self.update_img.source = self.check_list[self.s_index] # select file
-        
-        # move horizontally to the right
-        if self.moving_right:
-            self.x +=5 
+    # move horizontally to the right
+    def move_right(self):
+            self.x += 2 
             if self.x >= self.parent.width: # come back from the left if it goes right out of the screen
                 self.x = -self.width
-        
+    
+    # animations
+    # default jump
+    def default(self, dt): 
+        if not self.animating:
+            return
+        if self.moving_right:
+            self.move_right()
+        # Update image
+        self.update_img.source = self.check_list[self.s_index] # select file
         # plays the next frame from the png files
         self.s_index += 1 
         if self.s_index >= len(self.check_list)-0: # if number greater than the total number of files, 
             self.s_index = 0 # go back to 0
+
+    # working overtime 
+    def working(self, dt):
+        if not self.animating:
+            return
+        if self.moving_right:
+            self.move_right()
+        self.update_img.source = self.check_list[self.s_index] # select file
+        self.s_index += 1 
+        if self.s_index >= len(self.check_list)-0: 
+            self.s_index = 0 
+
 
     def toggle_mode(self, *args):
         # Cycle through 0 → 1 → 2 → 0 ...
@@ -94,15 +149,30 @@ class MainApp(App): # background screen
         super().__init__() 
         self.body = Screen(name="master_box") 
 
-        self.l_rloe = l_Rloe() # add character
+        # Add character
+        self.l_rloe = l_Rloe() 
         self.body.add_widget(self.l_rloe) 
 
+        # Add buttons to select animaitons
+        self.check_box_list = list()
+        c_b1 = Select_Button(pos=(-100, -100), text="Default", rotate=-25)
+        c_b2 = Select_Button(pos=(-100, -100), text="Working", rotate=-50)
+        c_b3 = Select_Button(pos=(-100, -100), text="Eating", rotate=-75)
+        self.body.add_widget(c_b1)
+        self.body.add_widget(c_b2)
+        self.body.add_widget(c_b3)
+        self.check_box_list.append(c_b1)
+        self.check_box_list.append(c_b2)
+        self.check_box_list.append(c_b3)
+
         Window.borderless = True 
+        Window.always_on_top = True
 
         # Bind all touch events
         Window.bind(on_touch_down=self.on_touch_down)
         Window.bind(on_touch_move=self.on_touch_move) # distinguish between clicking and dragging
         Window.bind(on_touch_up=self.on_touch_up)
+        Window.bind(on_keyboard = self.on_keyboard)
 
         self._touch_start_pos = None
         self._touch_start_time = 0 # add timing threshold to consider it a drag only if touch is too long
@@ -112,7 +182,26 @@ class MainApp(App): # background screen
         if self.l_rloe.collide_point(*touch.pos):
             self.l_rloe.toggle_mode()  # Cycle between move+animate → animate → freeze
 
+    def handle_right_click(self, touch):
+        center = self.l_rloe.center
+        height = self.l_rloe.height * 0.55
+        for i in self.check_box_list:
+            obst = Vector(0, height)
+            i.pos = obst.rotate(i.rotate) + center
+
     def on_touch_down(self, window, touch):
+        # Don't treat right click as normal click/drag
+        if hasattr(touch, "button"):
+            if touch.button == "right":
+                self.handle_right_click(touch)
+                return True  
+
+        # Left click / drag handling here
+        # If a button handled it, don't process further
+        for widget in self.body.children:
+            if isinstance(widget, Button) and widget.collide_point(*touch.pos):
+                return False  # Stop here so animation doesn't get the click
+        # Animation click logic    
         if self.l_rloe.collide_point(*touch.pos):
             self._touch_start_pos = touch.pos  # Store starting point
             self._touch_start_time = time.time()
@@ -130,7 +219,7 @@ class MainApp(App): # background screen
                 self.l_rloe.y = touch.y - self.l_rloe.height / 2
 
     def on_touch_up(self, window, touch):
-        if self.l_rloe.collide_point(*touch.pos):
+        if self._touch_start_pos is not None and self.l_rloe.collide_point(*touch.pos):
             duration = time.time() - self._touch_start_time
             dx = touch.pos[0] - self._touch_start_pos[0]
             dy = touch.pos[1] - self._touch_start_pos[1]
@@ -143,6 +232,11 @@ class MainApp(App): # background screen
 
         self._touch_start_pos = None
         self._is_dragging = False
+
+    def on_keyboard(self, window, key, *args):
+        #print(key)
+        if key == 27:
+            return True
 
 
     def build(self):
